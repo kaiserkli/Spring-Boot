d40d23a1e58c6bd69aa1445db0feeb658ac39e69
LT <lit@acsm.cn>
2020-07-17 11:10:51 +0800

    redis+lua做限流

---
+++ current-limiting/src/main/java/com/example/limiting/config/RedisLimiterHelper.java
@@ -0,0 +1,21 @@
+package com.example.limiting.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisLimiterHelper {
+
+    @Bean
+    public RedisTemplate limitRedisTemplate(LettuceConnectionFactory redisConnectionFactory) {
+        RedisTemplate template = new RedisTemplate<>();
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+}
---
+++ current-limiting/src/main/java/com/example/limiting/utils/LimitType.java
@@ -0,0 +1,8 @@
+package com.example.limiting.utils;
+
+public enum LimitType {
+    /** * 自定义key */
+    CUSTOMER,
+    /** * 请求者IP */
+    IP;
+}
